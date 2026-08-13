47e614662d21e4feaff50e10aca499e5a8cd4c2c
Hanna <anna.tarchenko94@gmail.com>
2026-08-14 01:44:04 +0200

    Add tank exercise

---
+++ Learning/tank.py
@@ -0,0 +1,13 @@
+
+def tank_erstellung ():  
+    tank = { 
+      "Laenge" : 5, 
+      "Durchmesser" : 1.6,
+      "Inhalt" : "Heizoel",
+      "Fuellhoehe" : 0, 
+      "Zustand" : "leer" 
+      } 
+    return tank 
+
+tank_erstellung()
+tank = tank_erstellung()
